f92c309199dee02c48e1937a6f26a43405f2aae0
ChrisMcElroy <csmcelroy@me.com>
2022-05-18 14:39:08 -0700

    Initial Commit

---
+++ barclock/barclockApp.swift
@@ -0,0 +1,17 @@
+//
+//  barclockApp.swift
+//  barclock
+//
+//  Created by Chris McElroy on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct barclockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
