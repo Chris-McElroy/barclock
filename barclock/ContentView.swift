69971ea5f59535179c0d24e5d21d889086cb7324
ChrisMcElroy <csmcelroy@me.com>
2022-10-31 23:28:56 +0000

    new cyan and new spacing

--- barclock/ContentView.swift
+++ barclock/ContentView.swift
@@ -16,11 +16,12 @@ struct ContentView: View {
     var body: some View {
 		VStack(spacing: 0) {
 			if settings.timer {
-				HStack {
+				HStack(spacing: 0) {
 					Spacer()
 					Circle()
 						.foregroundColor(getColor(time: settings.time - settings.timerOffset + 3, mod: 8, div: 180))
 						.border(.clear)
+						.aspectRatio(contentMode: .fit)
 						.onTapGesture {
 							if waitForTap() { return }
 							if (settings.time - settings.timerOffset)/180 % 8 == 0 && (settings.timerOffset - settings.time) % 180 != 0 {
@@ -29,9 +30,11 @@ struct ContentView: View {
 								settings.timerOffset += 180
 							}
 						}
+					circleStack
 					Circle()
 						.foregroundColor(getColor(time: settings.time - settings.timerOffset + 3, mod: 6, div: 30))
 						.border(.clear)
+						.aspectRatio(contentMode: .fit)
 						.onTapGesture {
 							if waitForTap() { return }
 //							if (time - timerOffset)/30 % 6 == 0 {
@@ -40,9 +43,11 @@ struct ContentView: View {
 							settings.timerOffset += 30
 //							}
 						}
+					circleStack
 					Circle()
 						.foregroundColor(getColor(time: settings.time - settings.timerOffset + 3, mod: 5, div: 6))
 						.border(.clear)
+						.aspectRatio(contentMode: .fit)
 						.onTapGesture {
 							if waitForTap() { return }
 //							if (time - timerOffset)/5 % 6 == 0 {
@@ -51,6 +56,8 @@ struct ContentView: View {
 							settings.timerOffset += 6
 //							}
 						}
+					circleStack
+						.frame(width: settings.minuteHand ? nil : 0)
 					Circle()
 						.foregroundColor(getColor(time: settings.time - settings.timerOffset + 3, mod: 6, div: 1))
 						.border(.clear)
@@ -68,18 +75,21 @@ struct ContentView: View {
 			} else {
 				Circle().fill(.clear)
 			}
-			Spacer().frame(height: 5)
-			HStack {
+			Spacer().frame(height: 4)
+			HStack(spacing: 0) {
 				Spacer()
 				Circle()
 					.foregroundColor(getColor(time: settings.time, mod: settings.dayMode ? 8 : 4, div: 180))
+					.aspectRatio(contentMode: .fit)
 //					.onTapGesture {
 //						if waitForTap() { return }
 //						settings.dayMode.toggle()
 //						StatusBarController.main.dayModeItem.state = settings.dayMode ? .on : .off
 //					}
+				circleStack
 				Circle()
 					.foregroundColor(getColor(time: settings.time, mod: 6, div: 30))
+					.aspectRatio(contentMode: .fit)
 //					.onTapGesture {
 //						if waitForTap() { return }
 //						if settings.timer {
@@ -91,15 +101,20 @@ struct ContentView: View {
 //							settings.timer = true
 //						}
 //					}
+				circleStack
 				Circle()
 					.foregroundColor(getColor(time: settings.time, mod: 6, div: 5))
+					.aspectRatio(contentMode: .fit)
 //					.onTapGesture {
 //						if waitForTap() { return }
 //						Settings.main.minuteHand.toggle()
 //						StatusBarController.main.minuteHandItem.state = Settings.main.minuteHand ? .on : .off
 //					}
+				circleStack
+					.frame(width: settings.minuteHand ? nil : 0)
 				Circle()
 					.foregroundColor(getColor(time: settings.time, mod: 5, div: 1))
+					.aspectRatio(contentMode: .fit)
 					.frame(width: settings.minuteHand ? nil : 0)
 				Spacer()
 			}
@@ -118,7 +133,7 @@ struct ContentView: View {
 			Color(.displayP3, red: 0.7, green: 0.0, blue: 0.0, opacity: 1.0),
 			Color(.displayP3, red: 1.0, green: 1.0, blue: 0.0, opacity: 1.0),
 			Color(.displayP3, red: 0.0, green: 0.7, blue: 0.0, opacity: 1.0),
-			Color(.displayP3, red: 0.0, green: 1.0, blue: 0.9, opacity: 1.0),
+			Color(.displayP3, red: 0.0, green: 0.95, blue: 0.9, opacity: 1.0),
 			Color(.displayP3, red: 0.05, green: 0.4, blue: 1.0, opacity: 1.0),
 			Color(.displayP3, red: 1.0, green: 0.2, blue: 1.0, opacity: 1.0),
 			Color(.displayP3, red: 1.0, green: 1.0, blue: 1.0, opacity: 1.0),
@@ -144,6 +159,16 @@ struct ContentView: View {
 		})
 		
 		return false
+	}
+	
+	var circleStack: some View {
+		VStack(spacing: 0) {
+			Circle().foregroundColor(.clear)
+			Circle().foregroundColor(.clear)
+			Circle().foregroundColor(.clear)
+			Circle().foregroundColor(.clear)
+		}
+		.aspectRatio(contentMode: .fit)
 	}
 }
 
